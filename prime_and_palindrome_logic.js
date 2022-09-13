42c1226eb98e32e19262913e68c4dc4311880ea6
Vishal Anand <vishalanand270@gmail.com>
2022-09-13 12:45:49 +0530

    problem of prime and palindrome added

---
+++ prime_and_palindrome_logic.js
@@ -0,0 +1,29 @@
+
+//prime problem
+
+let isPrime=true;
+
+let num=13;
+
+for(let i=0;i<num;i++){
+    isPrime=false;
+}
+
+if(isPrime==true){
+    console.log("The number is Prime.")
+}
+else{
+    console.log("The number is not Prime.")
+}
+
+
+//palindrome problem
+
+let str="naman";
+let str1;
+
+for(let i=0;i<str.length;i++){
+    str==str1
+}
+
+console.log("Palindrome");
